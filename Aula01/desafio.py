f63e0f0b351fdae8a072f24f6a2e50e0a57ad0e2
Lucas Costa <lucasrc.rodri@gmail.com>
2022-11-09 11:28:36 -0300

    adicionando o desafio da semana

---
+++ Aula01/desafio.py
@@ -0,0 +1,12 @@
+"""
+DESAFIO!
+11) Você terá umas férias maravilhosas que começam no dia 22 do mês 6. Você retornará das suas férias depois de 137 dias (Uauu!). Escreva um programa que pede o dia do mês e o mês do ano em que você irá viajar e pede ainda o número de dias que você ficará de férias e imprime o dia da semana que você voltará. (Considere que cada mês possui 30 dias). Não use biblioteca extra!
+
+Exemplo:
+Dia do mês: 22
+Mês do ano: 6
+Dias de férias: 137
+Você irá voltar no dia 9 do mês 11
+"""
+
+#desenvolva seu código:
